18a32c822bbd4cff958d15504a64e32bc8332667
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-10-31 18:53:12 +0530

    Added the backend

---
+++ Backend/main.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
+const app = express()
+const PORT = process.env.PORT
+const MONGODB_URL = process.env.MONGODB_URL
+app.use(express.json())
+// app.use("/api/v1/mentor")
+// app.use("/api/v1/mentee")
+app.get("/", function (req, res) {
+    res.json({ msg: "Hello world" })
+})
+
+async function main() {
+    try {
+        await mongoose.connect(MONGODB_URL)
+    } catch (err) {
+        console.log('Error while connecting');
+    }
+
+}
+app.listen(PORT, () => {
+    console.log('App is running at port', PORT);
+
+
+
+})
+main()
---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const {Router}=require("express")
+const mentorRouter=Router()
+const {z}=require("zod")
+const mongoose=require("mongoose")
+mentorRouter.get("/",function (req,res) {
+    res.status(200).json({msg:"You are on the main route"})
+})
+mentorRouter.post("/signup",function (req,res) {
+    const requiredData=z.object({
+        username:z.string().max().min(),
+        email:z.string().min().max(),
+        firstName:z.string().min().max(20),
+        lastName:z.string().min().max(20)
+        gender:
+    })
+    res.status(200).json({msg:"You are on the main route"})
+})
