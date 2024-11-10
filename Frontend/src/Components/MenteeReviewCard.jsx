fabd0f5833fcf1a87afec717e6015eeb24c7bace
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-10 23:51:50 +0530

    Added a new mentee review card component

---
+++ Frontend/src/Components/MenteeReviewCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function MenteeReviewCard(props) {
+    return (
+        <div className='bg-white border border-gray-400 rounded-md flex flex-col gap-4 p-4 w-fit'>
+            <div id='starRating'>{props.rating}</div>
+            <div id='reviewContent' className='font-montserrat'>
+                <div id="reviewTitle" className='text-[24px] '>{props.title}</div>
+                <div id="review" className='text-[16px]'>{props.review}</div>
+            </div>
+            <div id='userProfile' className='flex gap-2 items-center'>
+                <img className='rounded-full w-[61px] h-[61px]' src={props.userImg} alt="" />
+                <div id='userDetails' className='font-montserrat'>
+                    <h1 className='font-montserrat font-bold text-[17px]'>{props.username}</h1>
+                    <p className='text-[17px] font-medium text-gray-600'>{props.date}</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MenteeReviewCard
