9ae1a9af676585c4251c5b51ead988e8f3d7cdaf
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-11-10 23:52:37 +0530

    fix the text sizes

---
+++ Frontend/src/Components/PopularMentorCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PopularMentorCard = ({ name, position, src, description, bgColor }) => (
+  <div className={`w-[300px] h-[300px] ${bgColor} relative rounded-2xl`}>
+    <div id="name" className='font-montserrat font-medium text-[18px] relative top-[40%] left-[30%]'>
+      {name}
+    </div>
+    <div id="position" className='font-montserrat font-medium text-[13px] relative top-[40%] left-[30%] text-gray-800'>
+      {position}
+    </div>
+    <div id='image' className='relative left-[-40%]'>
+      <img src={src} alt="" />
+    </div>
+    <div id="description" className='w-[319px] h-[85px] rounded-xl bg-white font-montserrat text-left p-4 text-sm absolute left-[-50px] bottom-[-20px] shadow-xl'>
+      {description}
+    </div>
+  </div>
+);
+
+export default PopularMentorCard;
