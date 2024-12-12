cc13113d3b17a034b95116510db81aa8cdd86e59
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2024-12-12 22:00:42 +0530

    Adding signup and singin routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './screens/Home'
 import MentorFind from './screens/MentorFind'
 import MyMentors from './screens/MyMentors'
 import Resources from './screens/Resources'
+import Signin from './screens/Signin'
+import Signup from './screens/Signup'
 import Navbar from './Components/Navigation'
 
 const Routes = createBrowserRouter([{
@@ -15,19 +17,36 @@ const Routes = createBrowserRouter([{
 {
   path: "/mentors",
   element: <div>
+    <Navbar/>
     <MentorFind />
   </div>
 },
 {
   path: "/mentor",
   element: <div>
+    <Navbar/>
     <MyMentors />
   </div>
 },
 {
   path: "/resources",
   element: <div>
+    <Navbar/>
     <Resources />
+  </div>
+},
+{
+  path: "/signin",
+  element: <div>
+    <Navbar/>
+    <Signin />
+  </div>
+},
+{
+  path: "/signup",
+  element: <div>
+    <Navbar/>
+    <Signup />
   </div>
 },
 ])
