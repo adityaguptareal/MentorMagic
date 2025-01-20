2891ff4a17c3ac8c3881e65991c0e430df31c5c0
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2025-01-20 23:14:44 +0530

    Add routing structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './screens/Home'
 import MentorFind from './screens/MentorFind'
 import MyMentors from './screens/MyMentors'
@@ -7,52 +7,23 @@ import Resources from './screens/Resources'
 import Signin from './screens/Signin'
 import Signup from './screens/Signup'
 import Navbar from './Components/Navigation'
-
-const Routes = createBrowserRouter([{
-  path: "/",
-  element: <div>
-    <Home />
-  </div>
-},
-{
-  path: "/mentors",
-  element: <div>
-    <Navbar/>
-    <MentorFind />
-  </div>
-},
-{
-  path: "/mentor",
-  element: <div>
-    <Navbar/>
-    <MyMentors />
-  </div>
-},
-{
-  path: "/resources",
-  element: <div>
-    <Navbar/>
-    <Resources />
-  </div>
-},
-{
-  path: "/signin",
-  element: <div>
-    <Signin />
-  </div>
-},
-{
-  path: "/signup",
-  element: <div>
-    <Navbar/>
-    <Signup />
-  </div>
-},
-])
+import Layout from './Layout'
 
 function App() {
   return (
-    <RouterProvider router={Routes}></RouterProvider>
+    <>
+    <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mentor" element={<MyMentors />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Layout>
+      </BrowserRouter>
+    </>
   )
 }
 
